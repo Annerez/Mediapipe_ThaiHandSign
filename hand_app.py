35a97020a71ea2b5050d49d1d95702052c2dc235
Anner <98201562+Annerez@users.noreply.github.com>
2023-06-08 16:53:35 +0700

    Update hand_app.py

--- hand_app.py
+++ hand_app.py
@@ -6,24 +6,13 @@ import mediapipe as mp
 import av
 
 RTC_CONFIGURATION = RTCConfiguration(
-    iceServers=[
-        {"urls": ["stun:relay.metered.ca:80"]},
+    {"iceServers": [
         {
             "urls": ["turn:relay.metered.ca:80"],
             "username": "7c6e2dfc7ba5dd33578fc9e1",
             "credential": "18GkZDVEKpCweYAf",
-        },
-        {
-            "urls": ["turn:relay.metered.ca:443"],
-            "username": "7c6e2dfc7ba5dd33578fc9e1",
-            "credential": "18GkZDVEKpCweYAf",
-        },
-        {
-            "urls": ["turn:relay.metered.ca:443?transport=tcp"],
-            "username": "7c6e2dfc7ba5dd33578fc9e1",
-            "credential": "18GkZDVEKpCweYAf",
-        },
-    ]
+        }
+    ]}
 )
 
 mp_holistic = mp.solutions.holistic # Holistic model
