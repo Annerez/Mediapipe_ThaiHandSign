fca9e8275687e865cbade5299663782662ca143b
Anner <98201562+Annerez@users.noreply.github.com>
2023-06-13 16:48:54 +0700

    Update hand_app.py

--- hand_app.py
+++ hand_app.py
@@ -9,19 +9,12 @@ from utils import *
 from datetime import datetime
 import time
 
-actions = np.array(['สวัสดี', 'ตก', 'รอ', 'กลับ', 'ขอบคุณ', 'ตัด', 'ลง', 'ขึ้น', 'เฝ้า','คุย', 'ช่วย', 'เชื่อ', 'ฟัง', 'มอง', 'พา', 'ชวน หลีกภัย', 'ทักษิณ ชินวัตร', 'ขนม', 'พิธา ลิ้มเจริญรัตน์', 'ศิริกัญญา ตันสกุล','แบก', 'อนุทิน ชาญวีรกูล', 'รังสิมันต์ โรม', 'พีระพันธุ์ สาลีรัฐวิภาค', 'คุณหญิงสุดารัตน์ เกยุราพันธุ์', 'พลเอกอนุพงษ์ เผ่าจินดา', 'สุวัจน์ ลิปตพัลลภ', 'กรณ์ จาติกวณิช', 'วราวุธ ศิลปะอาชา', 'พล.ต.อ.เสรีพิศุทธ์ เตมียเวสวราวุธ ศิลปะอาชา', 'ศักดิ์สยาม ชิดชอบ', 'ชาดา ไทยเศรษฐ์', 'สุชัชวีร์ สุวรรณสวัสดิ์', 'จุรินทร์ ลักษณวิศิษฏ์', 'ไตรรงค์ สุวรรณคีรี', 'พลเอกประยุทธ์ จันทร์โอชา', 'นฤมล ภิญโญสินวัฒน์', 'ธรรมนัส พรหมเผ่า', 'ชัยวุฒิ ธนาคมานุสรณ์', 'ไพบูลย์ นิติตะวัน', 'พลเอกประวิตร วงษ์สุวรรณ', 'พริษฐ์ วัชรสินธุ', 'ยิ่งลักษณ์ ชินวัตร', 'ณัฐวุฒิ ใสยเกื้อ', 'นพ.ชลน่าน ศรีแก้ว', 'เศรษฐา ทวีสิน', 'แพทองธาร ชินวัตร', 'อะไร', 'สมัคร', 'กระโดด', 'ยก', 'ชน', 'ผ่าน', 'แนะนำ', 'จำได้', 'ปลูก', 'รวม', 'ทำหาย', 'เจอ', 'หาย', 'พนักงานขาย', 'นักเขียนโปรแกรม', 'พ่อครัว', 'เจ้าหน้าที่ตำรวจ', 'เนื้อลูกแกะ', 'เนื้อหมู', 'คุณสบายดีไหม', 'ดูเหมือน', 'วาง', 'อยู่', 'ก', 'ข', 'ค', 'ฆ', 'ต', 'ถ', 'ง', 'จ', 'ฉ', 'ช', 'ซ', 'ฏ', 'ฐ', 'ฑ', 'ฒ', 'ณ', 'ด', 'ท', 'ธ', 'น', 'บ', 'ป', 'ผ', 'ฝ', 'พ', 'ฟ', 'ภ', 'ม', 'ย', 'ร', 'ล', 'ว', 'ศ', 'ษ', 'ส', 'ห', 'ฬ', 'อ', 'ฮ'])
-
-model = load_model("action2.h5", actions)
-
 mp_holistic = mp.solutions.holistic
 mp_drawing = mp.solutions.drawing_utils
 
-# Function for image processing and prediction
-
-# Function to load and process video frames
 def process_video(video_path):
     cap = cv2.VideoCapture(video_path)
-    sequence , sentence , predictions , threshold = [] , [] , [] , 0.2
+    sequence , sentence , predictions , threshold = [] , [] , [] , 0.9
     frames = []
     with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
         while cap.isOpened():
@@ -50,8 +43,8 @@ def process_video(video_path):
                         else:
                             sentence.append(actions[np.argmax(res)])
 
-                if len(sentence) > 5:
-                    sentence = sentence[-5:]
+                if len(sentence) > 3:
+                    sentence = sentence[-3:]
 
             cv2.rectangle(image, (0, 0), (640, 40), -1)
             fontpath = "./angsana.ttc"
@@ -65,7 +58,6 @@ def process_video(video_path):
         cap.release()
     return frames
 
-# Helper function to save the uploaded video file
 def save_uploaded_file(video_file):
     temp_file = NamedTemporaryFile(delete=False)
     temp_file.write(video_file.read())
@@ -76,19 +68,16 @@ def display_video(frames):
     video_placeholder = st.empty()
 
     for frame in frames:
-        # Convert frame to RGB
+
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        # Create PIL image
         image = Image.fromarray(frame_rgb)
 
-        # Display the image in Streamlit
         video_placeholder.image(image)
 
         time.sleep(0.025)
 
 def download_predicted_video(frames):
-    # Convert frames to video
     output_path = "predicted_video.mp4"
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
     height, width, _ = frames[0].shape
@@ -100,16 +89,37 @@ def download_predicted_video(frames):
 
     out.release()
 
-    # Download the video file
     with open(output_path, "rb") as f:
         video_bytes = f.read()
 
     st.download_button("Download Predicted Video", data=video_bytes, file_name=f"processed_video_{timestamp}.mp4")
 
 def main():
-    st.title("Video Processing Web App")
+    st.title("Thai Sign Language Detection Model")
+    st.markdown("""---""")
+    st.subheader("เพื่อความแม่นยำของโมเดล กรุณาถ่ายวีดีโอแบบครึ่งตัว")
+    st.subheader("ดูตัวอย่างได้ที่ https://www.th-sl.com/")
+    global model
+    global actions
 
-    # Upload video file
+    large = "Large Model (110 words)"
+    medium = "Normal Model (45 words) (recommended)"
+    test = "Test Model (3 words)"
+    options = [test, medium, large]
+    app_mode = st.sidebar.selectbox("Choose the app mode", options)
+
+    st.subheader(app_mode)
+
+    if app_mode == medium:
+        actions = np.array(['สวัสดี', 'รอ', 'กลับ', 'ขอบคุณ', 'ลง', 'ขึ้น', 'เฝ้า','คุย', 'ช่วย', 'เชื่อ', 'ฟัง', 'มอง', 'พา', 'ทักษิณ ชินวัตร', 'ขนม','แบก', 'อะไร', 'สมัคร', 'กระโดด', 'ยก', 'ชน', 'ผ่าน', 'แนะนำ', 'จำได้', 'ปลูก', 'รวม', 'ทำหาย', 'เจอ', 'หาย', 'พนักงานขาย', 'นักเขียนโปรแกรม', 'พ่อครัว', 'เจ้าหน้าที่ตำรวจ', 'เนื้อลูกแกะ', 'เนื้อหมู', 'คุณสบายดีไหม', 'ดูเหมือน', 'วาง', 'อยู่', 'ก', 'ข', 'ค', 'ง', 'จ', 'ช', 'อ'])
+        model = load_model2("action4.h5", actions)
+    elif app_mode == large:
+        actions = np.array(['สวัสดี', 'ตก', 'รอ', 'กลับ', 'ขอบคุณ', 'ตัด', 'ลง', 'ขึ้น', 'เฝ้า','คุย', 'ช่วย', 'เชื่อ', 'ฟัง', 'มอง', 'พา', 'ชวน หลีกภัย', 'ทักษิณ ชินวัตร', 'ขนม', 'พิธา ลิ้มเจริญรัตน์', 'ศิริกัญญา ตันสกุล','แบก', 'อนุทิน ชาญวีรกูล', 'รังสิมันต์ โรม', 'พีระพันธุ์ สาลีรัฐวิภาค', 'คุณหญิงสุดารัตน์ เกยุราพันธุ์', 'พลเอกอนุพงษ์ เผ่าจินดา', 'สุวัจน์ ลิปตพัลลภ', 'กรณ์ จาติกวณิช', 'วราวุธ ศิลปะอาชา', 'พล.ต.อ.เสรีพิศุทธ์ เตมียเวสวราวุธ ศิลปะอาชา', 'ศักดิ์สยาม ชิดชอบ', 'ชาดา ไทยเศรษฐ์', 'สุชัชวีร์ สุวรรณสวัสดิ์', 'จุรินทร์ ลักษณวิศิษฏ์', 'ไตรรงค์ สุวรรณคีรี', 'พลเอกประยุทธ์ จันทร์โอชา', 'นฤมล ภิญโญสินวัฒน์', 'ธรรมนัส พรหมเผ่า', 'ชัยวุฒิ ธนาคมานุสรณ์', 'ไพบูลย์ นิติตะวัน', 'พลเอกประวิตร วงษ์สุวรรณ', 'พริษฐ์ วัชรสินธุ', 'ยิ่งลักษณ์ ชินวัตร', 'ณัฐวุฒิ ใสยเกื้อ', 'นพ.ชลน่าน ศรีแก้ว', 'เศรษฐา ทวีสิน', 'แพทองธาร ชินวัตร', 'อะไร', 'สมัคร', 'กระโดด', 'ยก', 'ชน', 'ผ่าน', 'แนะนำ', 'จำได้', 'ปลูก', 'รวม', 'ทำหาย', 'เจอ', 'หาย', 'พนักงานขาย', 'นักเขียนโปรแกรม', 'พ่อครัว', 'เจ้าหน้าที่ตำรวจ', 'เนื้อลูกแกะ', 'เนื้อหมู', 'คุณสบายดีไหม', 'ดูเหมือน', 'วาง', 'อยู่', 'ก', 'ข', 'ค', 'ฆ', 'ต', 'ถ', 'ง', 'จ', 'ฉ', 'ช', 'ซ', 'ฏ', 'ฐ', 'ฑ', 'ฒ', 'ณ', 'ด', 'ท', 'ธ', 'น', 'บ', 'ป', 'ผ', 'ฝ', 'พ', 'ฟ', 'ภ', 'ม', 'ย', 'ร', 'ล', 'ว', 'ศ', 'ษ', 'ส', 'ห', 'ฬ', 'อ', 'ฮ'])
+        model = load_model("action2.h5", actions)
+    elif app_mode == test:
+        actions = np.array(['สวัสดี', 'รอ', 'ขอบคุณ'])
+        model = load_model2("action6.h5", actions)
+
     video_file = st.file_uploader("Upload video file", type=["mp4"])
     if video_file is not None:
         video_path = save_uploaded_file(video_file)
