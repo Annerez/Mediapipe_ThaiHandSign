ccaa6010028540cc20fdc44a820bd40322a84192
Anner <98201562+Annerez@users.noreply.github.com>
2023-06-13 19:37:27 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,29 +1,26 @@
 import cv2
 import numpy as np
 import mediapipe as mp
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
-import os
+import sys
 
 mp_holistic = mp.solutions.holistic
 mp_drawing = mp.solutions.drawing_utils
 
 def mediapipe_detection(image, model):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image.flags.writeable = False
-    results = model.process(image)
-    image.flags.writeable = True
+    #image.flags.writeable = False
+    results = model.process(image.copy())
+    #image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     return image, results
 
-def draw_landmarks(image, results):
+def draw_landmarks(image, results, mp_drawing, mp_holistic):
     mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS)
     mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
     mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
     mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
 
-def draw_styled_landmarks(image, results):
-    # Draw face connections
+def draw_styled_landmarks(image, results, mp_drawing, mp_holistic):
     mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_CONTOURS, 
                              mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), 
                              mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
@@ -49,18 +46,21 @@ def extract_keypoints(results):
     face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
     lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
     rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
-    return np.concatenate([pose, face, lh, rh])
+    
+    output =  np.concatenate([pose, face, lh, rh])
+    print(f" Keypoints memory : {sys.getsizeof(output) * 1e-6} MB")
+    return output
 
 
 def load_model(model_path,actions):
     model = Sequential()
 
-    # Layers
     model.add(LSTM(256, return_sequences=False, activation='relu', input_shape=(30, 1662), dropout=0.2))
     model.add(Dense(109, activation='softmax'))
 
     model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
     model.load_weights(model_path)
+    print(f" Keras Model memory : {sys.getsizeof(model) * 1e-6} MB")
 
     return model
 
@@ -76,4 +76,5 @@ def load_model2(model_path,actions):
     model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
     model.load_weights(model_path)
 
+    return model
     return model
